6f6db394daae3b24eced16d595b73541c2f37d9b
Sahil pawar <63465844+sahilpawar58@users.noreply.github.com>
2024-01-28 20:45:50 +0530

    adding middleware to playlist to improve security

--- src/middlewares/privilege.middleware.js
+++ src/middlewares/privilege.middleware.js
@@ -1,9 +1,10 @@
+import { Playlist } from "../models/playlist.model.js";
 import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 
-export const isPrivileged = asyncHandler(async(req,res,next) => {
+const isPrivileged = asyncHandler(async(req,res,next) => {
     const { videoId } = req.params;
 
     let video  = await Video.findById(videoId);
@@ -17,3 +18,21 @@ export const isPrivileged = asyncHandler(async(req,res,next) => {
 
     throw new ApiError("Video does not belong to you",401);
 })
+
+const isPlaylistOwner = asyncHandler(async(req,res,next) => {
+    const { playlistId } = req.params;
+
+    let playlist  = await Playlist.findById(playlistId);
+    if(!playlist){
+        throw new ApiError("Playlist not Found");
+    }
+
+    if(req.user.id === playlist.owner){
+        next()
+    }
+
+    throw new ApiError("Playlist does not belong to you",401);
+})
+
+
+export {isPrivileged,isPlaylistOwner};
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -9,6 +9,7 @@ import {
     updatePlaylist,
 } from "../controllers/playlist.controller.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
+import {isPlaylistOwner} from "../middlewares/privilege.middleware.js"
 
 const router = Router();
 
@@ -19,11 +20,11 @@ router.route("/").post(createPlaylist)
 router
     .route("/:playlistId")
     .get(getPlaylistById)
-    .patch(updatePlaylist)
-    .delete(deletePlaylist);
+    .patch(isPlaylistOwner,updatePlaylist)
+    .delete(isPlaylistOwner,deletePlaylist);
 
-router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
-router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
+router.route("/add/:videoId/:playlistId").patch(isPlaylistOwner,addVideoToPlaylist);
+router.route("/remove/:videoId/:playlistId").patch(isPlaylistOwner,removeVideoFromPlaylist);
 
 router.route("/user/:userId").get(getUserPlaylists);
 
